fb651798bfc58fdc078ca1116cb9971d9df70578
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-03-14 11:51:44 +0530

    constants added

--- NoHunger/lib/app.dart
+++ NoHunger/lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:NoHunger/constants.dart';
 import 'package:NoHunger/screens/splash.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +12,14 @@ class MyApp extends StatelessWidget {
         textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(primary: Colors.black),
         ),
-        primaryColor: Colors.orange,
-        primarySwatch: Colors.orange,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.white))),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+            style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.black))),
+        primaryColor: pColor,
+        primarySwatch: pSwatch,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: SplashScreen(),
