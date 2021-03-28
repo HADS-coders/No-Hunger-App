8cb73151d24d2e68681ebdc60c1dff4a6d5d781b
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-03-28 18:13:26 +0530

    screen orientation fixed to potrait

--- NoHunger/lib/main.dart
+++ NoHunger/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MyApp());
 }
