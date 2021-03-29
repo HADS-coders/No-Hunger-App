4bc8b8f1f4ca39b600a522ea0487f1b7802b8bef
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-03-29 12:47:38 +0530

    text enclosed in fittedbox

--- NoHunger/lib/widgets/customButton.dart
+++ NoHunger/lib/widgets/customButton.dart
@@ -7,36 +7,40 @@ Widget customCircularButton(
     String subTitle,
     String img,
     void Function() onTap}) {
-  return Column(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      GestureDetector(
-        onTap: onTap,
-        child: CircleAvatar(
-          radius: screenWidth(context) / 6,
-          backgroundColor: pColor,
+  return Expanded(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        GestureDetector(
+          onTap: onTap,
           child: CircleAvatar(
-            radius: screenWidth(context) / 6 - 2,
-            backgroundColor: Colors.white,
-            backgroundImage: Image.asset(img).image,
+            radius: screenWidth(context) / 6,
+            backgroundColor: pColor,
+            child: CircleAvatar(
+              radius: screenWidth(context) / 6 - 2,
+              backgroundColor: Colors.white,
+              backgroundImage: Image.asset(img).image,
+            ),
           ),
         ),
-      ),
-      Container(
-        margin: EdgeInsets.all(5),
-        child: Text(
-          title,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-        ),
-      ),
-      Container(
+        Container(
           margin: EdgeInsets.all(5),
-          width: screenWidth(context) / 3,
-          child: Text(
-            subTitle,
-            style: TextStyle(fontWeight: FontWeight.w300, fontSize: 12),
-            textAlign: TextAlign.center,
-          ))
-    ],
+          child: FittedBox(
+            child: Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+        ),
+        Container(
+            margin: EdgeInsets.all(5),
+            width: screenWidth(context) / 3,
+            child: Text(
+              subTitle,
+              style: TextStyle(fontWeight: FontWeight.w300, fontSize: 12),
+              textAlign: TextAlign.center,
+            ))
+      ],
+    ),
   );
 }
