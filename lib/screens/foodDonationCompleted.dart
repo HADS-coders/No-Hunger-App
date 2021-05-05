8f862e070d73a970eb849f1f16a6d1d3cefa2c1e
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-05 11:22:00 +0530

    onwillpopscope added to false

--- lib/screens/foodDonationCompleted.dart
+++ lib/screens/foodDonationCompleted.dart
@@ -4,32 +4,37 @@ class FoodDonationCompleted extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Expanded(
-                child: Center(
-                  child: Text(
-                    'Food donation request successful.\nYou will receive a call from one of our volunteer regarding further process under 15 minutes.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      body: WillPopScope(
+        onWillPop: () async => false,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      'Food donation request successful.\nYou will receive a call from one of our volunteer regarding further process under 15 minutes.',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.popUntil(context, ModalRoute.withName('home'));
-                    },
-                    child: Text('Done'),
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30))),
-                  ))
-            ],
+                Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.popUntil(
+                            context, ModalRoute.withName('home'));
+                      },
+                      child: Text('Done'),
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30))),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/moneyDonationCompleted.dart
+++ lib/screens/moneyDonationCompleted.dart
@@ -4,32 +4,37 @@ class MoneyDonationCompleted extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Expanded(
-                child: Center(
-                  child: Text(
-                    'Donation Successfull.\n\nEach of your donation means a lot to a needy and we woud hope to see you again.\nReceipt will be mailed to your email soon.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      body: WillPopScope(
+        onWillPop: () async => false,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      'Donation Successfull.\n\nEach of your donation means a lot to a needy and we woud hope to see you again.\nReceipt will be mailed to your email soon.',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.popUntil(context, ModalRoute.withName('home'));
-                    },
-                    child: Text('Done'),
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30))),
-                  ))
-            ],
+                Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.popUntil(
+                            context, ModalRoute.withName('home'));
+                      },
+                      child: Text('Done'),
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30))),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
